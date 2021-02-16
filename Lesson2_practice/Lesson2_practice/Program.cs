1cef1b167f5b51b83b0e52798b3712af514a699c
Sergey <karbofos22@yandex.ru>
2021-02-16 19:17:56 +0300

    Homework

---
+++ Lesson2_practice/Lesson2_practice/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Lesson2_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1,2,5 - среднесуточная температура, вывод месяца и сообщение
+
+            Console.WriteLine("Введите минимальную температуру за сутки");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int middle = (min + max) / 2;
+            Console.WriteLine($"Среднесуточная температура: {middle} градус(ов)\n");
+
+            Console.WriteLine("Укажите номер текущего месяца");
+            int month = Convert.ToInt32(Console.ReadLine());
+            string monthnumber = "";
+            switch (month)
+            {
+                case 1:
+                    monthnumber = "Январь";
+                    break;
+                case 2:
+                    monthnumber = "Февраль";
+                    break;
+                case 3:
+                    monthnumber = "Март";
+                    break;
+                case 4:
+                    monthnumber = "Апрель";
+                    break;
+                case 5:
+                    monthnumber = "Май";
+                    break;
+                case 6:
+                    monthnumber = "Июнь";
+                    break;
+                case 7:
+                    monthnumber = "Июль";
+                    break;
+                case 8:
+                    monthnumber = "Август";
+                    break;
+                case 9:
+                    monthnumber = "Сентябрь";
+                    break;
+                case 10:
+                    monthnumber = "Октябрь";
+                    break;
+                case 11:
+                    monthnumber = "Ноябрь";
+                    break;
+                case 12:
+                    monthnumber = "Декабрь";
+                    break;
+                default:
+                    Console.WriteLine("Номер месяца указан не верно");
+                    break;
+
+            }
+            Console.WriteLine(monthnumber);
+            if (middle > 0 && month <= 12 && month >= 10 || month <= 2)
+                Console.WriteLine("Дождливая зима");
+        }
+    }
+}
---
+++ Lesson2_practice/additionalPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace additionalPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу, которая определяет является ли год високосным, и выводит сообщение в консоль.
+            //Каждый 4 - й год является високосным, кроме каждого 100 - го, при этом каждый 400 - й – високосный.
+
+            Console.WriteLine("Введите год, чтобы узнать високосный он или нет");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+            {
+                Console.WriteLine($"{year} - високосный год");
+            }
+            else
+                Console.WriteLine("Введенный год не является високосным");
+
+
+        }
+    }
+}
