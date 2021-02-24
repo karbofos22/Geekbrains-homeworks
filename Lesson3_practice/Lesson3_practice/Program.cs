946b7a81359dd47052a84df9d29cf780009fa75e
Sergey <karbofos22@yandex.ru>
2021-02-24 12:46:03 +0300

    added Homework files for lesson3

---
+++ Lesson3_practice/Lesson3_practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson3_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1 - Написать программу, выводящую элементы двумерного массива по диагонали.
+
+
+
+
+            int[,] mas = new int [6,6];
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    if (i == j)
+                        mas[i, j] = 8;
+                    Console.Write(mas[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
+
+        
---
+++ Lesson3_practice/Lesson_practice3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_practice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 3 - Написать программу, выводящую введённую пользователем строку в обратном порядке (olleH вместо Hello).
+
+            Console.WriteLine("Напишите что-нибудь, чтобы вывести надпись задом наперед");
+            string word = Console.ReadLine();
+            for (int i = word.Length -1; i >=0; i--)
+            {
+                Console.Write(word[i]);
+            }
+        }
+    }
+}
