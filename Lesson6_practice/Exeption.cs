bb93c59c454e3f130fc29a1727fb211a1d803291
Sergey <karbofos22@yandex.ru>
2021-03-16 14:27:44 +0300

    Added Lesson 6 homework

---
+++ Lesson6_practice/Exeption.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson6_practice
+{
+    public class ExceptionByLength : Exception { }
+    public class ExceptionByWrongElement : Exception { }
+
+}
