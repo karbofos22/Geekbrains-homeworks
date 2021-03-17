3d005419076f9a9c70434b03b6b87ffb64b0d0ab
Sergey <karbofos22@yandex.ru>
2021-03-17 18:36:16 +0300

    Added files for Homework8

---
+++ Lesson8_practice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using ClassLibrary1;  // созданная библиотека с приветствием
+
+
+namespace Lesson8_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            //1.Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и позволяет завершить указанный процесс. 
+            //    Предусмотреть возможность завершения процессов с помощью указания его ID или имени процесса. 
+            //    В качестве примера можно использовать консольные утилиты Windows tasklist и taskkill.
+
+            Process taskList = Process.Start("tasklist.exe");
+            Console.WriteLine("Введите Id процесса который нужно закрыть\n");
+            int id = Int32.Parse(Console.ReadLine());
+            Process.GetProcessById(id).Kill();
+
+            //SayHello.Greeting(); //Задание 2. Создать библиотеку с методом привествия, создать второе консольное приложение, которое выводит это приветствие.
+
+
+        }
+    }
+}
